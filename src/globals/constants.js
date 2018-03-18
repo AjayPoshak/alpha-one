e0bb097ed8e5c91c5b12aee18c03e585b8b8964d
Ajay Poshak <poshakajay@gmail.com>
2018-03-18 17:22:44 +0530

    create constants for global use

--- src/globals/constants.js
+++ src/globals/constants.js
@@ -1,1 +1,3 @@
-export const READING_LIST_URL = 'http://58f204ba33e3131200bdde85.mockapi.io/bookList'
+export const READING_LIST_URL = "http://58f204ba33e3131200bdde85.mockapi.io/bookList"
+export const BLOGS_LIST_URL = "http://58f204ba33e3131200bdde85.mockapi.io/blogslist"
+export const BIO_URL = "http://58f204ba33e3131200bdde85.mockapi.io/bio"

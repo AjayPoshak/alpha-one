8e75b72edba6cff1d539ed9b8d488925a26b33a0
Ajay Poshak <poshakajay@gmail.com>
2018-03-18 17:23:23 +0530

    Add entry to reducers, add utilities to helpers file

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux"
-import { readingList } from "../components/ReadingList/ReadingListReducer"
+import readingList from "../components/ReadingList/ReadingListReducer"
+import homePage from "../components/HomePage/HomePageReducer"
 
 export default combineReducers({
-	readingList
+	readingList,
+	homePage
 })

28c1a6805337f82c62bb8db598817648e04ee128
Ajay Poshak <poshakajay@gmail.com>
2018-03-24 12:50:50 +0530

    Update constants to reflect new URLs

--- src/globals/constants.js
+++ src/globals/constants.js
@@ -1,3 +1,3 @@
-export const READING_LIST_URL = "http://58f204ba33e3131200bdde85.mockapi.io/bookList"
-export const BLOGS_LIST_URL = "http://58f204ba33e3131200bdde85.mockapi.io/blogslist"
+export const READING_LIST_URL = "/api/reading"
+export const BLOGS_LIST_URL = "/api/home"
 export const BIO_URL = "http://58f204ba33e3131200bdde85.mockapi.io/bio"

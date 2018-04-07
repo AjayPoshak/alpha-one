09b77f481eec3ecfaf8a79b8fd5636f052a5df9d
Ajay Poshak <poshakajay@gmail.com>
2018-04-08 04:05:58 +0530

    Move hamburger image URL to constants file

--- src/globals/constants.js
+++ src/globals/constants.js
@@ -1,3 +1,5 @@
 export const READING_LIST_URL = "/api/reading"
 export const BLOGS_LIST_URL = "/api/home"
 export const BIO_URL = "http://58f204ba33e3131200bdde85.mockapi.io/bio"
+export const HAM_IMG_URL =
+	"http://res.cloudinary.com/ddbxa4afa/image/upload/v1521130892/ham_fsbm9r.png"
